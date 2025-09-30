5e2b8748b2eec768cbe052d8baf332d33217451d
swholmstead <swholmstead@gmail.com>
2025-09-30 16:07:27 -0400

    Update DistanceSensor.ino

--- DistanceSensor.ino
+++ DistanceSensor.ino
@@ -1,29 +1,32 @@
+
 #include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include <Adafruit_NeoPixel.h>
+#include <EEPROM.h>
 
+// pins for Ultrasonic distance sensor
 #define triggerPin    12
 #define echoPin       13
 
-long duration;
-int distance;
-
+// pins for LED strip
 #define ledPin        14
 #define numLEDs       30
 
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(numLEDs, ledPin, NEO_GRB + NEO_KHZ800);
+// pin for configuration button
+#define configPin     16
 
+// pins for OLED display
 #define sdaPin        4
 #define sclPin        5
 #define screenWidth   128
 #define screenHeight  64
 #define oledReset     -1
 
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(numLEDs, ledPin, NEO_GRB + NEO_KHZ800);
 Adafruit_SSD1306 display(screenWidth, screenHeight, &Wire, oledReset);
-
-
+int targetDistance = -1;
 
 // Arduino setup function. Runs in CPU 1
 void setup() {
@@ -32,6 +35,7 @@ void setup() {
   pinMode(triggerPin, OUTPUT);
   pinMode(echoPin, INPUT);
   digitalWrite(triggerPin, LOW);
+  pinMode(configPin, INPUT_PULLUP);
 
   pixels.begin();
   pixels.setBrightness(50); // to prevent overcurrent situation, start low
@@ -42,37 +46,47 @@ void setup() {
     while (1); // halt
   }
   display.clearDisplay();
-  display.setTextColor(WHITE);
-  display.setTextSize(1);
-  display.setCursor(0, 0);
-  display.println("Initializing...");
-  display.println("SSD1306 OLED");
-  display.drawRect(0, 20, 50, 30, WHITE);
   display.display();
 
-  delay(2000);
+  EEPROM.begin(4);
+  EEPROM.get(0, targetDistance);
+  Serial.printf("Target Distance: %d\n", targetDistance);
 }
 
 // Arduino loop function. Runs in CPU 1.
 void loop() {
-  static uint16_t hue = 0;
+  int distance = processDistance();
+  processDisplay(distance, targetDistance);
+  processPixels(distance, targetDistance);
+  processConfig(distance);
 
+  delay(100);
+}
+
+int processDistance() {
   // send 10 microsecond pulse to trigger of HC-SR04
   digitalWrite(triggerPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(triggerPin, LOW);
+  int distance = pulseIn(echoPin, HIGH) * .034 / 2;
+  Serial.printf("Ultrasonic distance: %d\n", distance);
+  return distance;
+}
 
-  duration = pulseIn(echoPin, HIGH);
-  distance = duration * .034 / 2;
+void processDisplay(int distance, int targetDistance) {
   Serial.printf("Distance: %d\n", distance);
   display.clearDisplay();
   display.setTextSize(1);
   display.setCursor(0, 0);
-  display.print("Parking Assistant");
+  display.println("Parking Assistant");
   display.print("Distance: ");
   display.setTextSize(2);
   display.printf("%d cm", distance);
   display.display();
+}
+
+void processPixels(int distance, int targetDistance) {
+  static uint16_t hue = 0;
 
   pixels.rainbow(hue, 20);
   pixels.show();
@@ -80,6 +94,11 @@ void loop() {
   if (hue >= 65535) {
     hue = 0;
   }
+}
 
-  delay(100);
+void processConfig(int distance) {
+  if (digitalRead(configPin) == LOW) {
+    EEPROM.put(0, distance);
+    Serial.printf("Stored target distance: %d\n", distance);
+  }
 }
